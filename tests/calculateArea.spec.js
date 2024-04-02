76018fc4ac9dc8629ff57493611e775e33dbb1a8
Stephan Granzow <stephan_granzow@web.de>
2024-04-02 16:51:37 +0200

    done

--- tests/calculateArea.spec.js
+++ tests/calculateArea.spec.js
@@ -1,14 +1,23 @@
 // UNCOMMENT THE CODE BELOW TO START
 
-/*
-
 describe("Iteration 3", () => {
-    describe("Function - calculateArea", () => {
-        
-        // Each `it` block represents 1 test. You can use the following as a template:
-        it("", () => {});
-
-    })    
-})
-
-*/
+  describe("Function - calculateArea", () => {
+    // Each `it` block represents 1 test. You can use the following as a template:
+    it("should be defined", () => {
+      expect(calculateArea).toBeDefined();
+    });
+    it("should take two arguments", () => {
+      expect(calculateArea.length).toBe(2);
+    });
+    it("should return the produkt of the two agruments", () => {
+      expect(calculateArea(2, 2)).toEqual(4);
+      expect(calculateArea(9, 3)).toEqual(27);
+      expect(calculateArea(9, 9)).toEqual(81);
+    });
+    it("should return undefined if any of the arguments is not provided", () => {
+      expect(calculateArea(1)).toEqual(undefined);
+      expect(calculateArea()).toEqual(undefined);
+      expect(calculateArea(undefined, 1)).toEqual(undefined);
+    });
+  });
+});
